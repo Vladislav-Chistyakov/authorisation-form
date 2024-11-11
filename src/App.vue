742ac7ffbda9e302cbdeb3a45af25eb867deffda
Vladislav_Chistyakov <vladischis@gmail.com>
2024-11-11 09:39:04 +0300

    authorization: init project

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+</script>
+
+<template>
+  <main class="main">
+    <div class="container">
+      <div class="wrapper">1</div>
+    </div>
+  </main>
+</template>
+
+<style lang="scss">
+@use '~/src/assets/main';
+@use '~/src/assets/sass/colors';
+
+* {
+  font-family: 'Inter', Helvetica, Arial, sans-serif;
+}
+
+body {
+  background-color: colors.$bg-main;
+}
+</style>
+
+<style scoped lang="scss">
+@use '~/src/assets/sass/colors';
+@use '~/src/assets/sass/container';
+@include container.container;
+
+.main {
+  background-color: colors.$bg-main;
+}
+
+.container {
+  background-color: colors.$text-lite-green;
+}
+
+.wrapper {
+  background-color: colors.$bg-block;
+}
+</style>
