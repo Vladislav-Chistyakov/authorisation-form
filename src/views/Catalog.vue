859f9aab25c7bc1476f211c0e40344ee34c7d3c4
Vladislav_Chistyakov <vladischis@gmail.com>
2024-12-01 11:51:04 +0300

    authorization: Вёрстка и стилизация каталога на мобилке

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -52,7 +52,10 @@ onBeforeMount(async () => {
       <li v-for="(item, index) in  list" :key="index" class="catalog__item">
         <RouterLink :to="`/catalog/${item.id}`" class="catalog__link">
 
-          <img :src="item.image.file.url" :alt="item.title" class="catalog__link-img" width="100" height="100" />
+          <div class="catalog__link-img-block" >
+            <img :src="item.image.file.url" :alt="item.title" class="catalog__link-img" />
+          </div>
+
 
           <div class="catalog__link-info">
             <strong class="catalog__link-title">{{ item.title }}</strong>
@@ -70,7 +73,7 @@ onBeforeMount(async () => {
     </div>
 
     <div v-if="error && !pending" class="catalog__error-block">
-      <strong class="catalog__error">{{ error }}</strong>
+      <strong class="catalog__error">ERROR: {{ error }}</strong>
     </div>
   </div>
 </template>
@@ -81,48 +84,81 @@ onBeforeMount(async () => {
 @include container.container;
 
 .catalog {
-  .catalog__heading {
-    margin: 0;
-  }
-
-  .catalog {
-  }
+  padding-top: 20px;
+  padding-bottom: 20px;
 
   .catalog__heading {
+    margin: 0 0 16px 0;
   }
 
   .catalog__list {
     margin: 0;
     padding: 0;
     list-style: none;
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 20px;
   }
 
   .catalog__item {
-    padding: 0;
+    background-color: colors.$bg-car;
+    padding: 12px 16px;
+    border-radius: 20px;
   }
 
-  .catalog__link {
+  .catalog__link-img-block {
+    padding: 10px;
+    border-radius: 16px;
+    overflow: hidden;
+    background-color: colors.$bg-block;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 12px;
   }
 
   .catalog__link-img {
+    object-fit: contain;
+    width: auto;
+    height: 200px;
+    max-width: 200px;
   }
 
   .catalog__link-info {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
   }
 
   .catalog__link-title {
+    font-size: 16px;
+    line-height: 18px;
+    color: colors.$text-grey;
   }
 
   .catalog__link-price {
+    font-size: 14px;
+    line-height: 16px;
+    color: colors.$text-grey;
   }
 
   .catalog__loading {
+    text-align: center;
+    padding: 20px 0;
   }
 
   .catalog__error-block {
+    display: flex;
+    padding: 20px;
+    border-radius: 20px;
+    background-color: colors.$bg-car;
+    margin-bottom: 20px;
   }
 
   .catalog__error {
+    font-size: 18px;
+    line-height: 20px;
+    color: #dc0000;
   }
 }
 </style>
